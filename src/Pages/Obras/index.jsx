da4eb1d71f8796441d4a418a0a59b7ceff331a92
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-08-04 21:20:36 -0300

    FIX/ Recomeçando

--- src/Pages/Obras/index.jsx
+++ src/Pages/Obras/index.jsx
@@ -76,86 +76,8 @@ export const Obras = () => {
     
     return (
         <>
-            <Header/>
-            <main className="main">
-                <div className="containerUsuario">
-                    <div className="containerUsuario-pesquisa" ref={containerPesquisaObras}>
-                        <div className="containerUsuario-pesquisa-barra">
-                            <div className="containerUsuario-pesquisa-barra-btn">
-                                <AiOutlineSearch onClick={pesquisarObra} className='containerUsuario-pesquisa-barra-loupe'/>
-                            </div>
-                            
-                            <input type="text" value={titulo} placeholder="Pesquisar" onChange={(e) => setTitulo(e.target.value)}/>
-                        </div>
-                        <div className='containerUsuario-pesquisa-obras'>
-                            
-
-                            {encontrado?(
-                                <>
-                                {
-                                    Obras?(
-                                        <>
-                                            <h1>Obras</h1>
-                                        {
-                                            Obras.map((item, index) => (
-                                                <div className='containerUsuario-pesquisa-mostrarObras' key={index}>
-                                                    <div className="containerUsuario-pesquisa-mostrarObras-titulos">
-                
-                                                        <h3>Titulo: {item.titulo}</h3>
-                
-                                                        <h3>Autores: {item.autores}</h3>
-                                                    </div>
-                
-                                                    <div className="containerUsuario-pesquisa-mostrarObras-resumo">
-                                                    <h3> Resumo: </h3>
-                                                        <p> { item.resumo } </p>
-                                                    </div>
-                                                
-                                                </div>
-                                            ))
-                                        }
-                                        </>
-                                    ): (
-                                        <CircularProgress/>
-                                    )
-                                }
-                                </>
-                            ):(
-                                <h1>Obra não encontrada!</h1>
-                            )}
-
-                                
-                            
-                            
-
-                        </div>
-                        
-                    </div>
-
-                    <div className="containerUsuario-obras">
-                        <div className="containerUsuario-obras-divisao">
-                            <div className="containerUsuario-obras-divisao-titulo">
-                                <h1>Titulo</h1>
-                            </div>
-                            <div className='containerUsuario-obras-divisao-conteudo'>
-                                {Carregando === true?(
-                                    Obras.map((item, index) => (
-                                        <div key={index} className="containerUsuario-obras-divisao-conteudo-titulo">
-                                            <h4 >{item.titulo}</h4>
-                                            {/* <h4 onClick={() => pesquisarObraTitulo(item._id)}>{item.titulo}</h4> */}
-                                        </div>
-
-                                    ))
-                                ): (
-                                    <CircularProgress/>
-                                )
-                                }
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </main>
+            
+        
         </>
     )
 }
